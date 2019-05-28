225260a9857c13566969ff91e4cc20ee2c083ba6
Steven Thornton <stevenwthornton0@gmail.com>
2019-05-28 17:38:32 -0500

    second commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$9rGHlJ1kb5m9ag6W9c9m1eDxeTr7b5BnfQDN/nR5uaTc3Pi8tolcm',
 }
