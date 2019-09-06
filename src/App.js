a8563da28d8af6bf7db5ddea4183a2a675a2ef64
Steven Thornton <stevenwthornton0@gmail.com>
2019-09-05 21:10:47 -0500

    update bookmark function added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import AddBookmark from './AddBookmark/AddBookmark';
+import EditBookmark from './EditBookmark/EditBookmark';
 import BookmarkList from './BookmarkList/BookmarkList';
+import BookmarksContext from './BookmarksContext';
 import Nav from './Nav/Nav';
 import config from './config';
 import './App.css';
-import BookmarksContext from './BookmarksContext'
-
 
 class App extends Component {
   state = {
@@ -30,7 +30,7 @@ class App extends Component {
   deleteBookmark = bookmarkId => {
     const newBookmarks = this.state.bookmarks.filter(bm =>
       bm.id !== bookmarkId
-      )
+    )
     this.setState({
       bookmarks: newBookmarks
     })
@@ -46,19 +46,31 @@ class App extends Component {
     })
       .then(res => {
         if (!res.ok) {
-          throw new Error(res.status)
+          return res.json().then(error => Promise.reject(error))
         }
         return res.json()
       })
       .then(this.setBookmarks)
-      .catch(error => this.setState({ error }))
+      .catch(error => {
+        console.error(error)
+        this.setState({ error })
+      })
+  }
+
+  updateBookmark = updatedBookmark => {
+    this.setState({
+      bookmarks: this.state.bookmarks.map(bm =>
+        (bm.id !== updatedBookmark.id) ? bm : updatedBookmark
+      )
+    })
   }
 
   render() {
     const contextValue = {
       bookmarks: this.state.bookmarks,
       addBookmark: this.addBookmark,
-      deleteBookmark: this.deleteBookmark
+      deleteBookmark: this.deleteBookmark,
+      updateBookmark: this.updateBookmark,
     }
     return (
       <main className='App'>
@@ -67,13 +79,17 @@ class App extends Component {
           <Nav />
           <div className='content' aria-live='polite'>
             <Route
+              exact
+              path='/'
+              component={BookmarkList}
+            />
+            <Route
               path='/add-bookmark'
               component={AddBookmark}
             />
             <Route
-              exact
-              path='/'
-              component={BookmarkList}
+              path='/edit/:bookmarkId'
+              component={EditBookmark}
             />
           </div>
         </BookmarksContext.Provider>
